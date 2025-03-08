418fabbb6b810d9a8fcb7e072412135240e277ca
ra2dd <radoslawpryw@gmail.com>
2025-03-09 00:40:19 +0100

    start server with custom host ip

--- server/lab1/Main.java
+++ server/lab1/Main.java
@@ -7,9 +7,10 @@ import java.net.MalformedURLException;
 import lab1.MyServerImpl;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Start");
+    public static void main(String[] args) throws Exception{
+        String host = initAndGetHost();
         int serverPort = 1099;
+
         try {
             // Uruchomienie rejestru na porcie
             LocateRegistry.createRegistry(serverPort);
@@ -19,11 +20,22 @@ public class Main {
             MyServerImpl server = new MyServerImpl();
 
             // Rejestracja obiektu w RMI
-            Naming.rebind("rmi://localhost/ABC", server);
-            System.out.println("Serwer RMI rozpoczął pracę.");
+            Naming.rebind("rmi://" + host + "/ABC", server);
+            System.out.println("Serwer RMI rozpoczął pracę na adresie " + host);
         } catch (RemoteException | MalformedURLException e) {
             System.err.println("Błąd podczas uruchamiania serwera RMI");
             e.printStackTrace();
         }
+    }
+
+    public static String initAndGetHost() throws Exception{
+        System.out.println("Initializing server...");
+        String hostEnvName = "RMI_SERVER_IP";
+        String host = System.getenv(hostEnvName);
+        if (host == null) {
+            throw new Exception("Ustaw zmienną środowiskową " + hostEnvName + ", aby uruchomić program.");
+        }
+        System.setProperty("java.rmi.server.hostname", host);
+        return host;
     }
 }
